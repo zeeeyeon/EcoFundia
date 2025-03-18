48b72b2487a961946c93ea025d8b8e2382811bb5
zeeeyeon <zeeyeonleee@gmail.com>
2025-03-18 16:17:00 +0900

    [S12P21E206-70] Feat: 프로젝트 구조 변경을 위한 폴더 리팩토링

--- back/funding/src/main/java/com/ssafy/funding/service/impl/FundingServiceImpl.java
+++ back/funding/src/main/java/com/ssafy/funding/service/impl/FundingServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ssafy.funding.service.impl;
 
 import com.ssafy.funding.service.FundingService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class FundingServiceImpl implements FundingService {
 }
