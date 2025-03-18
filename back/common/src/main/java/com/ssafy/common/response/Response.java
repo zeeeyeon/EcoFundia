a55da6906ae388c59e75b38a4e310c43134c9c40
이지연 <jiyeon_0342@naver.com>
2025-03-18 15:00:45 +0900

    [S12P21E206-47] Feat: 예외 및 응답 처리 기능 추가

---
+++ back/common/src/main/java/com/ssafy/common/response/Response.java
@@ -0,0 +1,27 @@
+package com.ssafy.common.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+public class Response<T> {
+    private Status status;
+    private T content;
+
+    @Getter
+    @AllArgsConstructor
+    private static class Status {
+        private int code;
+        private String message;
+    }
+
+    public static <T> Response<?> create(ResponseCode responseCode, T content) {
+        return Response.builder()
+                .status(new Status(responseCode.getCode(), responseCode.getMessage()))
+                .content(content)
+                .build();
+    }
+}
