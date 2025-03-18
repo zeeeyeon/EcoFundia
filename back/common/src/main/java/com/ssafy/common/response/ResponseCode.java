7078d1e76d412bcd37a72729c9ed97c3b32d8ffd
zeeeyeon <zeeyeonleee@gmail.com>
2025-03-18 15:00:45 +0900

    [S12P21E206-47] Feat: 예외 및 응답 처리 기능 추가

---
+++ back/common/src/main/java/com/ssafy/common/response/ResponseCode.java
@@ -0,0 +1,24 @@
+package com.ssafy.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ResponseCode {
+
+    SUCCESS_LOGIN(successCode(), HttpStatus.OK, "로그인이 성공적으로 완료되었습니다."),
+    SUCCESS_SIGNUP(successCode(), HttpStatus.OK, "회원가입이 성공적으로 완료되었습니다."),
+
+    BINDING_ERROR(400, HttpStatus.BAD_REQUEST, "입력값 중 검증에 실패한 값이 있습니다."),
+    BAD_REQUEST(400, HttpStatus.BAD_REQUEST, "올바르지 않은 요청입니다.");
+
+    private int code;
+    private HttpStatus httpStatus;
+    private String message;
+
+    private static int successCode() {
+        return 200;
+    }
+}
