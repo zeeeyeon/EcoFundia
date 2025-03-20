762682a627822b9869fce3f8d71d950b854f94ef
이지연 <jiyeon_0342@naver.com>
2025-03-20 16:55:19 +0900

    [S12P21E206-82] Feat: AWS S3 + 판매자 상세페이지 파일 전송 구현

--- back/funding/src/main/java/com/ssafy/funding/service/ProductService.java
+++ back/funding/src/main/java/com/ssafy/funding/service/ProductService.java
@@ -4,9 +4,12 @@ import com.ssafy.funding.dto.request.FundingCreateRequestDto;
 import com.ssafy.funding.dto.request.FundingUpdateRequestDto;
 import com.ssafy.funding.dto.response.FundingResponseDto;
 import com.ssafy.funding.entity.Funding;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface ProductService {
-    Funding createFunding(int sellerId, FundingCreateRequestDto dto);
+    Funding createFunding(int sellerId, FundingCreateRequestDto dto, MultipartFile storyFile, List<MultipartFile> imageFiles);
     FundingResponseDto getFunding(int fundingId);
     Funding updateFunding(int fundingId, FundingUpdateRequestDto dto);
     void deleteFunding(int fundingId);
