886887874698cc57a983eda71abfff0885a5e77b
dh0728 <dustkscjswo@naver.com>
2025-03-20 18:00:03 +0900

    [S12P21E206-56] [BE] Fixed: business 로직 대공사중

---
+++ back/business/src/main/java/com/ssafy/business/client/BusinessClient.java
@@ -0,0 +1,39 @@
+package com.ssafy.business.client;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "business")
+public interface BusinessClient {
+
+    // Top 펀딩 리스트 조회
+    @GetMapping("/api/business/top-funding")
+    ResponseEntity<?> getTopFundingList();
+
+    // 현재까지 펀딩 금액 조회
+    @GetMapping("/api/business/total-fund")
+    ResponseEntity<Long> getTotalFund();
+
+    // 최신 펀딩 리스트 조회
+    @GetMapping("api/business/latest-funding/{page}")
+    ResponseEntity<?> getLatestFundingList(@PathVariable int page);
+
+    // 카테고리별 펀딩 리스트 조회
+    @GetMapping("api/business/category")
+    ResponseEntity<?> getCategoryFundingList(@RequestParam(name="category") String category , @RequestParam(name="page") int page);
+
+    // 펀딩 키워드 검색 조회
+    @GetMapping("api/business/search")
+    ResponseEntity<?> getSearchFundingList(@RequestParam(name="keyword") String keyword, @RequestParam(name="page") int page);
+
+    // 펀딩 상세 페이지
+    @GetMapping("api/business/detail/{fundingId}")
+    ResponseEntity<?> getFundingDetail(@PathVariable int fundingId);
+
+    @GetMapping("api/business/review")
+    ResponseEntity<?> getFundingDetail(@RequestParam(name="sellerId") int sellerId, @RequestParam(name="page") int page);
+}
