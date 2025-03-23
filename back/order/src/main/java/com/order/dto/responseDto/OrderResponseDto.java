8fd980672229aaa5722b561508c0eae5345d1dcd
dh0728 <dustkscjswo@naver.com>
2025-03-23 16:14:37 +0900

    [S12P21E206-108] [BE] Feat: 주문 조회 구현중 + 병합전 커밋

---
+++ back/order/src/main/java/com/order/dto/responseDto/OrderResponseDto.java
@@ -0,0 +1,17 @@
+package com.order.dto.responseDto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrderResponseDto {
+
+    private int orderId;
+    private int userId;
+    private int fundingId;
+    private int amount;
+    private int quantity;
+    private int totalPrice;
+    private LocalDateTime createdAt;
+}
