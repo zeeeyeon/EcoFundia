e9d83b2a865147c94444b1602359df870ef06377
seowoo <dkr0210@naver.com>
2025-03-24 23:18:39 +0900

    [S12P21E206-80] Test: Jenkins 파이프라인 테스트

--- back/gateway/src/main/java/com/ssafy/gateway/config/CorsGlobalConfiguration.java
+++ back/gateway/src/main/java/com/ssafy/gateway/config/CorsGlobalConfiguration.java
@@ -13,7 +13,7 @@ public class CorsGlobalConfiguration {
     public CorsWebFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("http://localhost:64913");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
