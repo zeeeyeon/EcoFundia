3479aa6700ee199c8055cefee9f0fc25adcdc906
박수민 <psm5927@naver.com>
2025-03-25 21:35:09 +0900

    [S12P21E206-141] Fix: 에러코드수정

--- back/user/src/main/java/com/ssafy/user/common/response/ResponseCode.java
+++ back/user/src/main/java/com/ssafy/user/common/response/ResponseCode.java
@@ -13,12 +13,23 @@ public enum ResponseCode {
     LOGOUT_SUCCESS("SU", HttpStatus.OK, "로그아웃 성공"),
     GET_WISHLIST("SU", HttpStatus.OK, "위시리스트를 성공적으로 조회했습니다."),
     DELETE_WISHLIST("SU", HttpStatus.OK, "위시리스트에서 삭제되었습니다."),
+    LOGIN_SUCCESS("SU", HttpStatus.OK, "로그인에 성공했습니다."),
+    REISSUE_SUCCESS("SU", HttpStatus.OK, "액세스토큰이 재발급 되었습니다."),
+    GET_MYINFO("SU", HttpStatus.OK, "내정보를 불러오는데 성공했습니다."),
+    UPDATE_MYINFO("SU", HttpStatus.OK, "내정보를 수정하는데 성공했습니다."),
+    GET_MY_FUNDING_SUCCESS("GF", HttpStatus.OK, "내 펀딩 상세 조회 성공"),
+    GET_MY_TOTAL_FUNDING_SUCCESS("GT", HttpStatus.OK, "내 총 펀딩 금액 조회 성공"),
+    GET_MY_REVIEW_SUCCESS("GR", HttpStatus.OK, "내 리뷰 조회 성공"),
+    UPDATE_MY_REVIEW_SUCCESS("UR", HttpStatus.OK, "리뷰 수정 성공"),
+    DELETE_MY_REVIEW_SUCCESS("DR", HttpStatus.OK, "리뷰 삭제 성공"),
 
 
     // 생성 응답 (201)
     CREATED("CR", HttpStatus.CREATED, "Row Created successfully"),
-    CREATE_WISHLIST("CR", HttpStatus.OK, "위시리스트에 추가되었습니다."),
-
+    CREATE_USER("CR", HttpStatus.CREATED, "회원가입에 성공하였습니다."),
+    CREATE_WISHLIST("CR", HttpStatus.CREATED, "위시리스트에 추가되었습니다."),
+    CREATE_MY_REVIEW_SUCCESS("CR", HttpStatus.CREATED, "리뷰 작성 성공"),
+    CREATE_PAYMENT_SUCCESS("CP", HttpStatus.CREATED, "결제 생성 성공"),
 
     // 클라이언트 오류 (400)
     BAD_REQUEST("BR", HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
