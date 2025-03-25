95ed10871fe0712b9910ba437842224d12fb081e
박수민 <psm5927@naver.com>
2025-03-25 11:41:42 +0900

    [S12P21E206-112] Fix: 토큰처리 수정

--- back/user/src/main/java/com/ssafy/user/controller/WishListController.java
+++ back/user/src/main/java/com/ssafy/user/controller/WishListController.java
@@ -12,26 +12,26 @@ import java.util.List;
 import static com.ssafy.user.common.response.ResponseCode.*;
 
 @RestController
-@RequestMapping("/api/wishlist")
+@RequestMapping("/api/user/wishlist")
 @RequiredArgsConstructor
 public class WishListController {
 
     private final WishListService wishListService;
 
     @PostMapping("/{fundingId}")
-    public ResponseEntity<?> createWish(@RequestHeader("X-User-Id") int userId, @PathVariable int fundingId) {
+    public ResponseEntity<?> createWish(@RequestHeader("X-User-Id") String userId, @PathVariable int fundingId) {
         wishListService.createWish(userId, fundingId);
         return new ResponseEntity<>(Response.create(CREATE_WISHLIST, null), CREATE_WISHLIST.getHttpStatus());
     }
 
-    @GetMapping
-    public ResponseEntity<?> getMyWishlist(@RequestHeader("X-User-Id") int userId) {
+    @GetMapping("/onging")
+    public ResponseEntity<?> getMyWishlist(@RequestHeader("X-User-Id") String userId) {
         List<WishList> list = wishListService.getWishList(userId);
         return new ResponseEntity<>(Response.create(GET_WISHLIST, list), GET_WISHLIST.getHttpStatus());
     }
 
     @DeleteMapping("/{fundingId}")
-    public ResponseEntity<?> deleteWish(@RequestHeader("X-User-Id") int userId, @PathVariable int fundingId) {
+    public ResponseEntity<?> deleteWish(@RequestHeader("X-User-Id") String userId, @PathVariable int fundingId) {
         wishListService.deleteWish(userId, fundingId);
         return new ResponseEntity<>(Response.create(DELETE_WISHLIST, null), DELETE_WISHLIST.getHttpStatus());
     }
