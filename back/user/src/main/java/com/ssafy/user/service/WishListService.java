24d0ed5eacbe4054c626be11fa1758553911ab08
박수민 <psm5927@naver.com>
2025-03-25 11:41:42 +0900

    [S12P21E206-112] Fix: 토큰처리 수정

--- back/user/src/main/java/com/ssafy/user/service/WishListService.java
+++ back/user/src/main/java/com/ssafy/user/service/WishListService.java
@@ -5,8 +5,8 @@ import com.ssafy.user.entity.WishList;
 import java.util.List;
 
 public interface WishListService {
-    void createWish(int userId, int fundingId);
-    List<WishList> getWishList(int userId);
-    void deleteWish(int userId, int fundingId);
+    void createWish(String userId, int fundingId);
+    List<WishList> getWishList(String userId);
+    void deleteWish(String userId, int fundingId);
 }
 
