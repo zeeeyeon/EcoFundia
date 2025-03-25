f8e5f952e0f9e19d03187ee58f1ac9138961a7fd
박수민 <psm5927@naver.com>
2025-03-25 15:28:49 +0900

    [S12P21E206-116] Fix: CORS설정 수정

--- back/gateway/src/main/java/com/ssafy/gateway/config/CorsGlobalConfiguration.java
+++ back/gateway/src/main/java/com/ssafy/gateway/config/CorsGlobalConfiguration.java
@@ -1,27 +1,27 @@
-package com.ssafy.gateway.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
-
-@Configuration
-public class CorsGlobalConfiguration {
-
-    @Bean
-    public CorsWebFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true); // 인증 정보 허용
-        config.setAllowedOrigins(List.of("http://localhost:59343"));
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return new CorsWebFilter(source);
-    }
-}
-
+//package com.ssafy.gateway.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.reactive.CorsWebFilter;
+//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+//
+//import java.util.List;
+//
+//@Configuration
+//public class CorsGlobalConfiguration {
+//
+//    @Bean
+//    public CorsWebFilter corsFilter() {
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true); // 인증 정보 허용
+//        config.setAllowedOrigins(List.of("http://localhost:59343"));
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsWebFilter(source);
+//    }
+//}
+//
