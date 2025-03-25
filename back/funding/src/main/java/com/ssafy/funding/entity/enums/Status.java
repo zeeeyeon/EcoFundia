ef48676001f9356ec441e7d9a2b7cfee5669114f
zeeeyeon <zeeyeonleee@gmail.com>
2025-03-25 14:06:59 +0900

    [S12P21E206-135] Feat: Enum, JSON 내부 필드 JsonFormat 설정

--- back/funding/src/main/java/com/ssafy/funding/entity/enums/Status.java
+++ back/funding/src/main/java/com/ssafy/funding/entity/enums/Status.java
@@ -1,5 +1,8 @@
 package com.ssafy.funding.entity.enums;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum Status {
     ONGOING, SUCCESS, FAIL, CANCEL
 }
