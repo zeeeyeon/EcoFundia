0caee9c595d21391f43628e63a29c6a68b215243
박수민 <psm5927@naver.com>
2025-03-26 11:53:46 +0900

    [S12P21E206-112] Fix: 위시리스트dto 이름 수정

--- back/user/src/main/java/com/ssafy/user/service/WishListService.java
+++ back/user/src/main/java/com/ssafy/user/service/WishListService.java
@@ -1,15 +1,15 @@
 package com.ssafy.user.service;
 
 import com.ssafy.user.common.response.PageResponse;
-import com.ssafy.user.dto.response.WishiListResponseDto;
+import com.ssafy.user.dto.response.WishListResponseDto;
 
 public interface WishListService {
 
     void createWish(int userId, int fundingId);
 
-    PageResponse<WishiListResponseDto> getWishList(int userId, int page, int size);
+    PageResponse<WishListResponseDto> getWishList(int userId, int page, int size);
 
     void deleteWish(int userId, int fundingId);
 
-    PageResponse<WishiListResponseDto> getDoneWishList(int userId, int page, int size);
+    PageResponse<WishListResponseDto> getDoneWishList(int userId, int page, int size);
 }
