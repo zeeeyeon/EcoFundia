c2059c5f5dcbb6a2fef2bc50f5369c41b7bf5c07
dh0728 <dustkscjswo@naver.com>
2025-03-27 16:20:32 +0900

    [S12P21E206-174] Feat: order서비스 내가 주문한 펀딩 조회 order쪽 구현

--- back/order/src/main/java/com/order/client/FundingClient.java
+++ back/order/src/main/java/com/order/client/FundingClient.java
@@ -1,13 +1,20 @@
 package com.order.client;
 
+import com.order.dto.funding.response.FundingResponseDto;
 import com.order.dto.funding.response.IsOngoingResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @FeignClient(name = "funding" )
 public interface FundingClient {
 
     @GetMapping("api/funding/is-ongoing/{fundingId}")
     IsOngoingResponseDto isOngoing(@PathVariable int fundingId);
+
+    @GetMapping("api/funding")
+    List<FundingResponseDto> getMyFunding(@RequestBody List<Integer> fundingIds);
 }
