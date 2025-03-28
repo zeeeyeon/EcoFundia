47523add020ca264d0d2fb9bd51a73e7ed6f1b37
zeeeyeon <zeeyeonleee@gmail.com>
2025-03-28 12:18:49 +0900

    [S12P21E206-168] Refactor: 코드 리팩토링

--- back/seller/src/main/java/com/seller/service/impl/SellerServiceImpl.java
+++ back/seller/src/main/java/com/seller/service/impl/SellerServiceImpl.java
@@ -20,12 +20,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static com.seller.common.response.ResponseCode.SELLER_NOT_FOUND;
 
 @Slf4j
 @Service
@@ -36,48 +34,44 @@ public class SellerServiceImpl implements SellerService {
     private final FundingClient fundingClient;
     private final S3FileService s3FileService;
 
-//    @Override
-//    public ResponseEntity<?> getFundingId(int fundingId) {
-//        return fundingClient.getFundingId(fundingId);
-//    }
-//
-//    @Override
-//    public ResponseEntity<?> createFunding(int sellerId, FundingCreateRequestDto dto,
-//                                           MultipartFile storyFile, List<MultipartFile> imageFiles) {
-//        String storyFileUrl = s3FileService.uploadFile(storyFile, "funding/story");
-//        List<String> imageUrls = s3FileService.uploadFiles(imageFiles, "funding/images");
-//
-//        String imageUrlsJson = JsonConverter.convertImageUrlsToJson(imageUrls);
-//
-//        FundingCreateSendDto toDto = dto.toDto(storyFileUrl, imageUrlsJson);
-//
-//        return fundingClient.createFunding(sellerId, toDto);
-//    }
-//
-//    @Override
-//    public ResponseEntity<?> updateFunding(int fundingId, FundingUpdateRequestDto dto,
-//                                           MultipartFile storyFile, List<MultipartFile> imageFiles) {
-//
-//        String newStoryFileUrl = s3FileService.uploadFile(storyFile, "funding/story");
-//        List<String> newImageUrls = s3FileService.uploadFiles(imageFiles, "funding/images");
-//
-//        FundingResponseDto oldFunding = fundingClient.getFundingById(fundingId);
-//        String oldStoryFileUrl = oldFunding.storyFileUrl();
-//        List<String> oldImageUrls = oldFunding.imageUrlList();
-//
-//        if (newStoryFileUrl != null && !newStoryFileUrl.equals(oldStoryFileUrl)) s3FileService.deleteFile(oldStoryFileUrl);
-//        if (!newImageUrls.isEmpty() && !newImageUrls.equals(oldImageUrls)) s3FileService.deleteFiles(oldImageUrls);
-//
-//        String imageUrlsJson = JsonConverter.convertImageUrlsToJson(newImageUrls);
-//        FundingUpdateSendDto updateDto = dto.toDto(newStoryFileUrl, imageUrlsJson);
-//
-//        return fundingClient.updateFunding(fundingId, updateDto);
-//    }
-//
-//    @Override
-//    public ResponseEntity<?> deleteFunding(int fundingId) {
-//        return fundingClient.deleteFunding(fundingId);
-//    }
+
+    @Override
+    public ResponseEntity<?> createFunding(int sellerId, FundingCreateRequestDto dto,
+                                           MultipartFile storyFile, List<MultipartFile> imageFiles) {
+        String storyFileUrl = s3FileService.uploadFile(storyFile, "funding/story");
+        List<String> imageUrls = s3FileService.uploadFiles(imageFiles, "funding/images");
+
+        String imageUrlsJson = JsonConverter.convertImageUrlsToJson(imageUrls);
+
+        FundingCreateSendDto toDto = dto.toDto(storyFileUrl, imageUrlsJson);
+
+        return fundingClient.createFunding(sellerId, toDto);
+    }
+
+    @Override
+    public ResponseEntity<?> updateFunding(int fundingId, FundingUpdateRequestDto dto,
+                                           MultipartFile storyFile, List<MultipartFile> imageFiles) {
+
+        String newStoryFileUrl = s3FileService.uploadFile(storyFile, "funding/story");
+        List<String> newImageUrls = s3FileService.uploadFiles(imageFiles, "funding/images");
+
+        FundingResponseDto oldFunding = fundingClient.getFundingById(fundingId);
+        String oldStoryFileUrl = oldFunding.storyFileUrl();
+        List<String> oldImageUrls = oldFunding.imageUrlList();
+
+        if (newStoryFileUrl != null && !newStoryFileUrl.equals(oldStoryFileUrl)) s3FileService.deleteFile(oldStoryFileUrl);
+        if (!newImageUrls.isEmpty() && !newImageUrls.equals(oldImageUrls)) s3FileService.deleteFiles(oldImageUrls);
+
+        String imageUrlsJson = JsonConverter.convertImageUrlsToJson(newImageUrls);
+        FundingUpdateSendDto updateDto = dto.toDto(newStoryFileUrl, imageUrlsJson);
+
+        return fundingClient.updateFunding(fundingId, updateDto);
+    }
+
+    @Override
+    public ResponseEntity<?> deleteFunding(int fundingId) {
+        return fundingClient.deleteFunding(fundingId);
+    }
 
     @Override
     public Boolean findByUserId(int userId) {
@@ -95,7 +89,6 @@ public class SellerServiceImpl implements SellerService {
                         (existing, replacement) -> existing
                 ));
     }
-
 
     // 펀딩 상세페이지 seller 정보 조회
     @Transactional
