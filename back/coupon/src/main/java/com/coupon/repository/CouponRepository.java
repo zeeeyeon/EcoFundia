24c12dfc3930ac8c5fcd769b016ef5775871f521
zeeeyeon <zeeyeonleee@gmail.com>
2025-04-01 17:15:06 +0900

    [S12P21E206-201] Feat: 데이터베이스 비관적 락 걸기 (+ 쿠폰 과발급 수정)

--- back/coupon/src/main/java/com/coupon/repository/CouponRepository.java
+++ back/coupon/src/main/java/com/coupon/repository/CouponRepository.java
@@ -1,11 +1,19 @@
 package com.coupon.repository;
 
 import com.coupon.entity.Coupon;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CouponRepository extends JpaRepository<Coupon, Integer> {
     Optional<Coupon> findByCouponCode(int couponCode);
     boolean existsByCouponCode(int couponCode);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT c FROM Coupon c WHERE c.couponCode = :couponCode")
+    Optional<Coupon> findByCouponCodeWithLock(@Param("couponCode") int couponCode);
 }
