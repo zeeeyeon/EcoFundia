851edfda8ed8159bd8f7c92b812bdeeda9f89d62
dh0728 <dustkscjswo@naver.com>
2025-04-02 17:59:48 +0900

    [S12P21E206-193] Feat: 채팅 Kafka 구현

---
+++ back/chat/src/main/java/com/chat/config/KafkaAdminConfig.java
@@ -0,0 +1,21 @@
+package com.chat.config;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaAdminConfig {
+
+    @Bean
+    public AdminClient kafkaAdminClient() {
+
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "3.35.168.156:9092");
+        return AdminClient.create(configs);
+    }
+}
--- back/chat/src/main/java/com/chat/controller/ChatController.java
+++ back/chat/src/main/java/com/chat/controller/ChatController.java
@@ -2,7 +2,10 @@ package com.chat.controller;
 
 import com.chat.dto.ChatMessageDocument;
 import com.chat.dto.ChatMessageDto;
+import com.chat.producer.ChatKafkaProducer;
 import com.chat.repository.ChatMessageRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -16,21 +19,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
 
     // 특정 사용자에게 메시지를 보내는데 사용되는 STOMP을 이용한 템플릿
-    private final SimpMessagingTemplate template;
-    private final ChatMessageRepository chatMessageRepository;
+    // 카프카 쓸거니까 주석
+    // private final SimpMessagingTemplate template;
+    //private final ChatMessageRepository chatMessageRepository;
+
+    private final ChatKafkaProducer chatKafkaProducer;
+    private final ObjectMapper objectMapper;
 
     //  엔드 포인트로 데이터와 함께 호출하면 "sub/chat/{fundingId}" 를 수신하는 사용자에게 메세지를 전달합니다.
     @MessageMapping("/chat/{fundingId}")
-    public ChatMessageDto send2(@DestinationVariable(value = "fundingId") int fundingId, ChatMessageDto chatMessageDto){
+    public void handleMessage(@DestinationVariable(value = "fundingId") int fundingId, ChatMessageDto chatMessageDto) throws JsonProcessingException {
 
         log.info("MessageDto: {}", chatMessageDto);
+
+        String json = objectMapper.writeValueAsString(chatMessageDto);
+        chatKafkaProducer.send(fundingId, json);
 
         // MongoDB에 저장
         //ChatMessageDocument document = ChatMessageDocument.toDto(chatMessageDto);
         //chatMessageRepository.save(document);
 
-        template.convertAndSend("/sub/chat/" + fundingId , chatMessageDto);
-        return chatMessageDto;
     }
 
 }
