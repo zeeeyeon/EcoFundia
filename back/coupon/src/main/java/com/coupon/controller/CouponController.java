d0c648c675f4f3de959eca656ac43b0d581f2243
zeeeyeon <zeeyeonleee@gmail.com>
2025-04-03 22:44:31 +0900

    [S12P21E206-201] Feat: 쿠폰 발급 서비스,Redis lua script + DB insert 기능 구현

--- back/coupon/src/main/java/com/coupon/controller/CouponController.java
+++ back/coupon/src/main/java/com/coupon/controller/CouponController.java
@@ -1,10 +1,14 @@
 package com.coupon.controller;
 
 import com.coupon.common.response.Response;
+import com.coupon.common.util.CouponUtil;
+import com.coupon.service.CouponRedisService;
 import com.coupon.service.CouponService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 import static com.coupon.common.response.ResponseCode.ISSUED_COUPON;
 
@@ -13,11 +17,11 @@ import static com.coupon.common.response.ResponseCode.ISSUED_COUPON;
 @RequiredArgsConstructor
 public class CouponController {
 
-    private final CouponService couponService;
+    private final CouponRedisService couponRedisService;
 
     @PostMapping("/issue")
-    public ResponseEntity<?> issueCoupon(@RequestHeader("X-User-Id") int userId) {
-        couponService.issueCoupon(userId);
+    public ResponseEntity<?> issueCoupon(@RequestHeader("X-User-Id") int userId) throws IOException {
+        couponRedisService.issueCoupon(userId, CouponUtil.generateTodayCode());
         return new ResponseEntity<>(Response.create(ISSUED_COUPON, null), ISSUED_COUPON.getHttpStatus());
     }
 
