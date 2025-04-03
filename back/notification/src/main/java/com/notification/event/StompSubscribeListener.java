7eaa656814734aa87442b75028a4a771dd6b9906
dh0728 <dustkscjswo@naver.com>
2025-04-03 16:49:13 +0900

    [S12P21E206-225] Rename: 채팅코드 옮기는 중

---
+++ back/notification/src/main/java/com/notification/event/StompSubscribeListener.java
@@ -0,0 +1,11 @@
+package com.notification.event;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StompSubscribeListener {
+
+    private final
+}
