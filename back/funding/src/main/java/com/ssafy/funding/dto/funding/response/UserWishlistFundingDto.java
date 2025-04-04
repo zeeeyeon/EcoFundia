0464a505be9620e15176595a8d1e28e7fc81e411
이지연 <jiyeon_0342@naver.com>
2025-04-04 12:04:19 +0900

    [S12P21E206-231] Feat : 종료된 펀딩 위시리스트 조회 수정

--- back/funding/src/main/java/com/ssafy/funding/dto/funding/response/UserWishlistFundingDto.java
+++ back/funding/src/main/java/com/ssafy/funding/dto/funding/response/UserWishlistFundingDto.java
@@ -16,13 +16,17 @@ public record UserWishlistFundingDto(
         String sellerName
 ) {
     public static UserWishlistFundingDto from(Funding funding, String sellerName) {
-        int rate = (int) ((double) funding.getCurrentAmount() / funding.getTargetAmount() * 100);
+        int targetAmount = funding.getTargetAmount();
+        int rate = targetAmount == 0 ? 0 :
+                (int) ((double) funding.getCurrentAmount() / targetAmount * 100);
+
         int remainingDays = (int) ChronoUnit.DAYS.between(LocalDateTime.now(), funding.getEndDate());
-        int amountGap = funding.getCurrentAmount() - funding.getTargetAmount();
+        int amountGap = funding.getCurrentAmount() - targetAmount;
 
         List<String> imageUrls = funding.getImageUrlList();
-        System.out.println(imageUrls);
-        String imageUrl = imageUrls.isEmpty() ? "https://ssafy-funding.s3.ap-northeast-2.amazonaws.com/funding/images/1743560407748_%EB%8A%90%EC%A2%8B%EC%9A%B0%EB%A6%AC%EC%A1%B0.png" : imageUrls.get(0);
+        String imageUrl = (imageUrls == null || imageUrls.isEmpty())
+                ? "https://ssafy-funding.s3.ap-northeast-2.amazonaws.com/funding/images/1743560407748_%EB%8A%90%EC%A2%8B%EC%9A%B0%EB%A6%AC%EC%A1%B0.png"
+                : imageUrls.get(0);
 
         return new UserWishlistFundingDto(
                 funding.getFundingId(),
