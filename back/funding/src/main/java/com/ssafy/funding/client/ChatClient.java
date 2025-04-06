fc0c376616428f4fbaac89d69e247fe43dbbcb42
dh0728 <dustkscjswo@naver.com>
2025-04-07 00:23:34 +0900

    [S12P21E206-237] [BE] Fix: funding 오류 수정

--- back/funding/src/main/java/com/ssafy/funding/client/ChatClient.java
+++ back/funding/src/main/java/com/ssafy/funding/client/ChatClient.java
@@ -1,15 +1,15 @@
-//package com.ssafy.funding.client;
-//
-//import com.ssafy.funding.dto.chat.request.ChatRoomCreateRequest;
-//import com.ssafy.funding.dto.chat.response.ChatRoomCreateResponse;
-//import org.springframework.cloud.openfeign.FeignClient;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//
-//@FeignClient(name = "chat")
-//public interface ChatClient {
-//
-//    // 채팅방 생성
-//    @PostMapping("/api/chatroom")
-//    ChatRoomCreateResponse createRoom(@RequestBody ChatRoomCreateRequest request);
-//}
+package com.ssafy.funding.client;
+
+import com.ssafy.funding.dto.chat.request.ChatRoomCreateRequest;
+import com.ssafy.funding.dto.chat.response.ChatRoomCreateResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "chat")
+public interface ChatClient {
+
+    // 채팅방 생성
+    @PostMapping("/api/chatroom")
+    ChatRoomCreateResponse createRoom(@RequestBody ChatRoomCreateRequest request);
+}
