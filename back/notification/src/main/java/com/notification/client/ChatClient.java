135cc7d0f2124596e25ebd0ad04c53e2d380c863
dh0728 <dustkscjswo@naver.com>
2025-04-06 16:12:07 +0900

    [S12P21E206-233] Feat : 채팅참여시 테이블에 사용자 추가 로직 연결

--- back/notification/src/main/java/com/notification/client/ChatClient.java
+++ back/notification/src/main/java/com/notification/client/ChatClient.java
@@ -2,7 +2,9 @@ package com.notification.client;
 
 
 import com.notification.dto.ChatMessageDto;
+import com.notification.dto.request.AddParticipantRequest;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,4 +17,10 @@ public interface ChatClient {
 
     @PostMapping("/api/chat/{fundingId}/store")
     void storeMessages(@PathVariable int fundingId, @RequestBody List<ChatMessageDto> messages);
+
+    // 채팅방 참여자 추가
+    @PostMapping("/{fundingId}/participants")
+    ResponseEntity<?> addParticipant(@PathVariable int fundingId,
+                                            @RequestBody AddParticipantRequest request);
+
 }
