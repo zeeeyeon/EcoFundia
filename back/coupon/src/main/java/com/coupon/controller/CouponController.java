141484dcfe6d65c192fe723eb2f7e1b7fbdcd21c
이지연 <jiyeon_0342@naver.com>
2025-04-07 14:57:42 +0900

    [S12P21E206-243] Feat: 발급받은 쿠폰, 결제 로직에 적용시키기

--- back/coupon/src/main/java/com/coupon/controller/CouponController.java
+++ back/coupon/src/main/java/com/coupon/controller/CouponController.java
@@ -39,4 +39,14 @@ public class CouponController {
     public List<CouponResponseDto> getCoupons(@RequestHeader("X-User-Id") int userId) throws IOException {
         return couponService.getCoupons(userId);
     }
+
+    @GetMapping("/info")
+    public CouponResponseDto getCouponInfo(int couponId) throws IOException {
+        return couponService.getCouponInfo(couponId);
+    }
+
+    @PostMapping("/use")
+    public ResponseEntity<?> useCoupon(int userId, int couponId) throws IOException {
+        return couponService.useCoupon(userId, couponId);
+    }
 }
